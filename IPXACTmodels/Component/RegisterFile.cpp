1845030f3435fb46bbc52d3423cdb62c12e73887
Dan Chianucci <chianucci@xelic.com>
2018-06-04 17:59:41 -0400

    Made Register Base Contain the Dimension and address Offset Because it is shared between the register and registerFile Elements

--- IPXACTmodels/Component/RegisterFile.cpp
+++ IPXACTmodels/Component/RegisterFile.cpp
@@ -20,9 +20,7 @@
 // Function: RegisterFile::RegisterFile()
 //-----------------------------------------------------------------------------
 RegisterFile::RegisterFile(QString const& name, QString const& addressOffset, QString const& range):
-RegisterBase(name),
-dimension_(),
-addressOffset_(addressOffset),
+RegisterBase(name, addressOffset),
 range_(range),
 registerData_(new QList<QSharedPointer<RegisterBase> > ())
 {
@@ -34,8 +32,6 @@ registerData_(new QList<QSharedPointer<RegisterBase> > ())
 //-----------------------------------------------------------------------------
 RegisterFile::RegisterFile(const RegisterFile& other):
 RegisterBase(other),
-dimension_(other.dimension_),
-addressOffset_(other.addressOffset_),
 range_(other.range_),
 registerData_(new QList<QSharedPointer<RegisterBase> > ())
 {
@@ -50,10 +46,7 @@ RegisterFile& RegisterFile::operator=(const RegisterFile& other)
     if (this != &other)
     {
         RegisterBase::operator=(other);
-        dimension_ = other.dimension_;
-        addressOffset_ = other.addressOffset_;
         range_ = other.range_;
-        
         registerData_->clear();
         copyRegisterData(other);
     }
@@ -69,37 +62,6 @@ QSharedPointer<RegisterBase> RegisterFile::clone()
     return QSharedPointer<RegisterBase>(new RegisterFile(*this));
 }
 
-//-----------------------------------------------------------------------------
-// Function: RegisterFile::()
-//-----------------------------------------------------------------------------
-QString RegisterFile::getDimension() const
-{
-    return dimension_;
-}
-
-//-----------------------------------------------------------------------------
-// Function: RegisterFile::setDimension()
-//-----------------------------------------------------------------------------
-void RegisterFile::setDimension(QString const& newDimension)
-{
-    dimension_ = newDimension;
-}
-
-//-----------------------------------------------------------------------------
-// Function: RegisterFile::getAddressOffset()
-//-----------------------------------------------------------------------------
-QString RegisterFile::getAddressOffset() const
-{
-    return addressOffset_;
-}
-
-//-----------------------------------------------------------------------------
-// Function: RegisterFile::setAddressOffset()
-//-----------------------------------------------------------------------------
-void RegisterFile::setAddressOffset(QString const& newAddressOffset)
-{
-    addressOffset_ = newAddressOffset;
-}
 
 //-----------------------------------------------------------------------------
 // Function: RegisterFile::getRange()
